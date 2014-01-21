61f2f14daae9426787e5a4180731628d0251fa9b
gijigae <gijigae@gmail.com>
2014-01-21 23:07:55 +0900

    Intialize repository for the first rails app that is following steps laid out in ruby.railstutorial.org

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RtFirstApp::Application.config.secret_key_base = 'edbc7024aabcaac72df352f7126d9b4768d5062898ce4e3eaac4d4b69bb391581371cb89adb46dac2de82b8c7cb9ddcfe39a2ab6f6a2e25fbf60d18d3f860388'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RtFirstApp::Application.config.session_store :cookie_store, key: '_rt_first_app_session'
